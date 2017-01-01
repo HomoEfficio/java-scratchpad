c75fff0d50232a9807a91f59760dd9006b2f3f01
HomoEfficio <homo.efficio@gmail.com>
2017-01-01 17:42:52 +0900

    덧셈 추가

---
+++ src/main/java/day02/Algebra.java
@@ -0,0 +1,12 @@
+package day02;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2017-01-01.
+ */
+public class Algebra {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/day02/AlgebraTest.java
@@ -0,0 +1,19 @@
+package day02;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2017-01-01.
+ */
+public class AlgebraTest {
+
+    @Test public void addTest() {
+        Algebra algebra = new Algebra();
+        assertThat("1 + 2 = 3", algebra.add(1, 2), is(equalTo(3)));
+    }
+}
