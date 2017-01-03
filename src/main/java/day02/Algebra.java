1e5f54b0319868ac892066154553bc5b7be529a4
Oh Myung Woon <omwomw@sk.com>
2017-01-03 20:56:12 +0900

    뺄셈 추가

---
+++ src/main/java/day02/Algebra.java
@@ -0,0 +1,12 @@
+package day02;
+
+/**
+ * @author omwomw@sk.com
+ *         created on 2017. 1. 3.
+ */
+public class Algebra {
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/day02/AlgebraTest.java
@@ -0,0 +1,20 @@
+package day02;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author omwomw@sk.com
+ *         created on 2017. 1. 3.
+ */
+public class AlgebraTest {
+
+    @Test public void subtractTest() {
+
+        Algebra algebra = new Algebra();
+        assertThat("3 - 1 = 2", algebra.subtract(3, 1), is(equalTo(2)));
+    }
+}
