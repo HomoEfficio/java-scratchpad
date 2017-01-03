94c650bdbf9439d46092d0fceb3ad60598e68be3
Oh Myung Woon <omwomw@sk.com>
2017-01-04 08:35:36 +0900

    덧셈 a, b로

--- src/main/java/day02/Algebra.java
+++ src/main/java/day02/Algebra.java
@@ -10,7 +10,7 @@ public class Algebra {
         return c - d;
     }
 
-    public int add(int c, int d) {
-        return c + d;
+    public int add(int a, int b) {
+        return a + b;
     }
 }
